fd2dab733a2f4129f1f47d799bd41436b596650b
zeroboo <tanhung.vn@gmail.com>
2026-03-08 23:52:35 +0700

    arrange models to dedicated files

--- payment/model/transaction.go
+++ payment/model/transaction.go
@@ -23,7 +23,7 @@ const (
 
 // Transaction represents a single paymen
 type Transaction struct {
-	ID             string            `json:"id" db:"transaction_id"`
+	ID             string            `json:"transaction_id" db:"transaction_id"`
 	UserID         string            `json:"user_id" db:"user_id"`
 	Amount         float64           `json:"amount" db:"amount"`
 	Type           TransactionType   `json:"type" db:"type"`
@@ -32,4 +32,10 @@ type Transaction struct {
 	IdempotencyKey string            `json:"idempotency_key" db:"idempotency_key"` // same as TransactionID for this service
 	CreatedAt      time.Time         `json:"created_at" db:"created_at"`
 	UpdatedAt      time.Time         `json:"updated_at" db:"updated_at"`
+}
+
+type TransactionLock struct {
+	ID        string    `json:"transaction_id" db:"transaction_id"`
+	CreatedAt time.Time `json:"created_at" db:"created_at"`
+	DeletedAt time.Time `json:"deleted_at" db:"deleted_at"`
 }
